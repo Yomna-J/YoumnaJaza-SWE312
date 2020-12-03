a1db5e6fa2d5f63a986077cfc2eb78109c0132f5
Yomna-J <61107570+Yomna-J@users.noreply.github.com>
2020-12-04 00:02:34 +0300

    Update SigningIn.java
    - Add static modifier.

--- Main/src/main/java/components/SigningIn.java
+++ Main/src/main/java/components/SigningIn.java
@@ -16,18 +16,14 @@ import java.util.logging.Logger;
 public class SigningIn {
 
     /**
-     * An object of <b>DBConnection</b> class.
-     */
-    /**
      * Checks if the credentials of the user are correct by using SQL query.
      *
      * @param accountNum the user's account number.
      * @param password the user account password.
      * @return true if the credentials are correct; false otherwise.
      */
-    public boolean signIn(int accountNum, String password) {
+    public static boolean signIn(int accountNum, String password) {
         Connection connection = DBConnection.connectDB();
-
         int userId = getUserId(accountNum);
         if (connection != null && userId != 0) {
             try {
@@ -56,9 +52,8 @@ public class SigningIn {
      * @param accountNum the user account number
      * @return true if the account number exists; false otherwise.
      */
-    public boolean isRigesteredAccountNum(int accountNum) {
+    public static boolean isRigesteredAccountNum(int accountNum) {
         Connection connection = DBConnection.connectDB();
-
         if (connection != null) {
             try {
                 PreparedStatement statment = (PreparedStatement) connection.prepareStatement("SELECT account_num FROM accounts WHERE account_num = ?");
@@ -85,9 +80,8 @@ public class SigningIn {
      * @param accountNum the user account number.
      * @return string value of the email that belongs to that account number.
      */
-    public String getEmail(int accountNum) {
+    public static String getEmail(int accountNum) {
         Connection connection = DBConnection.connectDB();
-
         int userId = getUserId(accountNum);
         if (connection != null && userId != 0) {
             try {
@@ -120,9 +114,8 @@ public class SigningIn {
      * @param accountNum the user account number.
      * @return integer value of the user id that belongs to that account number.
      */
-    private int getUserId(int accountNum) {
+    private static int getUserId(int accountNum) {
         Connection connection = DBConnection.connectDB();
-
         int userId = 0;
         if (connection != null) {
             try {
