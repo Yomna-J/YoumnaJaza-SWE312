60fe775d4aea82e088907993c922c2fce65aa220
Yomna-J <61107570+Yomna-J@users.noreply.github.com>
2020-12-03 18:50:05 +0300

    Update DBConnection.java
    - Add javaDoc for the class.
    - Change varaibles modifiers to private.

--- Main/src/main/java/components/DBConnection.java
+++ Main/src/main/java/components/DBConnection.java
@@ -1,18 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package components;
 
 import java.sql.*;
 
+/**
+ * A class that establishes connection with a database on a local host.
+ *
+ * @author youmna
+ *
+ */
 public class DBConnection {
 
-    static final String DB_URL = "jdbc:mysql://localhost/banking_sys";
-    static final String USERNAME = "root";
-    static final String PASS = "";
-       
+    /**
+     * The URL to the database.
+     */
+    private static final String DB_URL = "jdbc:mysql://localhost/banking_sys";
+    /**
+     * The user name of the database to sign in.
+     */
+    private static final String USERNAME = "root";
+    /**
+     * The password of the database to sign in.
+     */
+    private static final String PASS = "";
+
+    /**
+     * Establish a connection with the database
+     *
+     * @return an object of @see {@link Connection}Connection class if the
+     * connection is successful; null otherwise;
+     */
     public static Connection connectDB() {
         Connection connection = null;
         try {
@@ -23,6 +39,5 @@ public class DBConnection {
             return null;
         }
     }
-    
-    
+
 }
