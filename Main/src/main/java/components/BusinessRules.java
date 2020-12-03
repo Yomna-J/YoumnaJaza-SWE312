a32f3ed5855fc67ef7570866fae3dbe5ed065745
Yomna-J <61107570+Yomna-J@users.noreply.github.com>
2020-12-03 18:31:40 +0300

    Update BusinessRules.java
    - Add JavaDoc to the class.
    - Change modifiers to static.

--- Main/src/main/java/components/BusinessRules.java
+++ Main/src/main/java/components/BusinessRules.java
@@ -1,38 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *The package contains the main components of the Banking system
  */
 package components;
 
 /**
- *
- * @author youmna
+ * Contains the business rules specified by the bank
  */
 public class BusinessRules {
 
-    private double maxWithdraw = 5000.0;
-    private double minWithdraw = 50.0;
-    private double maxDeposit = 12000.0;
-    private double minDeposit = 100.0;
-    private double maxTransfer = 10000.0;
-    private double minTransfer = 1000.0;
-    private int numOfAllowedTransactions = 5;
+    /**
+     * The maximum double amount of money per each withdrawal operation.
+     */
+    private final static double maxWithdraw = 5000.0;
+    /**
+     * The minimum double amount of money per each withdrawal operation.
+     */
+    private final static double minWithdraw = 50.0;
+    /**
+     * The maximum double amount of money per each deposit operation.
+     */
+    private final static double maxDeposit = 12000.0;
+    /**
+     * The minimum double amount of money per each deposit operation.
+     */
+    private final static double minDeposit = 100.0;
+    /**
+     * The maximum double amount of money per each transfer operation.
+     */
+    private final static double maxTransfer = 10000.0;
+    /**
+     * The minimum double amount of money per each transfer operation.
+     */
+    private final static double minTransfer = 1000.0;
+    /**
+     * The maximum number of transactions per day.
+     */
+    private final static int numOfAllowedTransactions = 5;
 
-    public boolean isValidWithdraw(double balance, double amount) {
+    /**
+     * Checks if the user's balance is larger than the amount.
+     *
+     * @param balance a user account's balance.
+     * @param amount amount to withdraw.
+     * @return true if the withdrawal is allowed; false otherwise.
+     */
+    public static boolean isValidWithdraw(double balance, double amount) {
         return balance >= amount && amount <= maxWithdraw && amount >= minWithdraw;
     }
 
-    public boolean isValidTransfer(double balance, double amount) {
+    /**
+     * Checks if the user's balance is larger than the amount.
+     *
+     * @param balance a user account's balance.
+     * @param amount amount to transfer.
+     * @return true if the transfer is allowed; false otherwise.
+     */
+    public static boolean isValidTransfer(double balance, double amount) {
 
         return balance >= amount && amount <= maxTransfer && amount >= minTransfer;
     }
 
-    public boolean isValidTransaction(int numOfTransactions) {
+    /**
+     * Checks if this number of transactions is valid according to the banking
+     * rules.
+     *
+     * @param numOfTransactions number of transactions per day
+     * @return true if number of transactions is less than or equal number of
+     * allowed transactions; false otherwise.
+     */
+    public static boolean isValidTransaction(int numOfTransactions) {
         return numOfTransactions < numOfAllowedTransactions;
     }
 
-    public boolean isValidDeposit(double amount) {
+    /**
+     * Checks if the amount meets the banking rules.
+     *
+     * @param amount amount to deposit.
+     * @return true if the amount meets the banking rules; false otherwise.
+     */
+    public static boolean isValidDeposit(double amount) {
         return amount <= maxDeposit && amount >= minDeposit;
     }
 }
